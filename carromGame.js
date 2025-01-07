2308cfac497e954bf31514272a239bc79bd5ab30
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-07 15:25:05 +0530

    added ineer line of canvas

---
+++ carromGame.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+
+
+
+
+  textAlign(CENTER,CENTER);
+  stroke(0,102,102);
+  strokeWeight(3);
+  fill(255,178,102);
+  square(400, 400, 555);
+}
